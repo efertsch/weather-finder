a84b9f652e87ce826e8befae9879d9c83ad77ebc
[01;31m[K"name"[m[K: "Ethan Fertsch <efertsch@gmail.com>
2018-11-11 16:14:50 -0500

    Refactors components

--- src/components/form.js
+++ src/components/form.js
@@ -1,15 +1,11 @@
 import React from 'react'
 
-class Form extends React.Component{
-    render(){
-        return(
-            <form onSubmit={this.props.getWeather}>
-                <input type="text" name="zipcode" placeholder="zipcode..."></input>
-                <input type="text" name="country" placeholder="country..."></input>
-                <button>Submit</button>
-            </form>
-        );
-    }
-}
+const Form = props => (  
+    <form onSubmit={props.getWeather}>
+        <input type="text" name="zipcode" placeholder="zipcode..."></input>
+        <input type="text" name="country" placeholder="country..."></input>
+        <button>Submit</button>
+    </form>
+);
 
 export default Form;
